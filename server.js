27cd6c49ea055b57a618aadda6a13e416fd830b4
HardikAswal <hardik.aswal@gmail.com>
2020-09-14 09:50:40 +0530

    Deployed

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ io.on('connection', function(socket) {
 // });
 
 
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT;
 // const port = 6000 || process.env.PORT;
 // app.listen(PORT , ()=>console.log(`Server started on PORT ${PORT}`));
 server.listen(PORT, ()=>console.log(`Socket server started on PORT ${PORT}`));
